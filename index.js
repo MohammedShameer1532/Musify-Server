996a3818dca3e09a7f67c500b9011930af47f09b
Mohammed Shameer <mshameer260@gmail.com>
2024-12-13 22:23:51 +0530

    add cors

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ require('./db/database');
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://musify-client-eta.vercel.app/home",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
