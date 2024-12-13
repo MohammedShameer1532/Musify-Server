aa79cb8b048c67628efa564313aadeea1b96d33a
Mohammed Shameer <mshameer260@gmail.com>
2024-12-14 00:16:08 +0530

    add true

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(session({
     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24, // 1 day
     sameSite: 'None',
-    secure: process.env.NODE_ENV === 'production',
+    secure: true,
   },
 
 }));
